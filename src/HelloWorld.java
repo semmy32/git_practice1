de7d30b81cc6b3732b3f8ac7953e60276200692e
Semra.G <semra.kavasgenc@gmail.com>
2022-03-31 15:30:15 +0100

    work 2 is done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,7 @@ public class HelloWorld {
     // I am learning Git, and it looks easier than Java, at least I hope so :)
 
     // Doing awesome work 2!
+    // Writing silly things here is my new homework now
+    // I hope this will get more interesting
 
 }
