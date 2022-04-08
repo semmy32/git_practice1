75a2a1cf8beffdac12ab9337469f352344371a9c
Semra.G <semra.kavasgenc@gmail.com>
2022-04-08 19:12:12 +0100

    power file modified

--- src/Day01_practice.java
+++ src/Day01_practice.java
@@ -1,7 +1,9 @@
 public class Day01_practice {
-    // Today is first day of Git lssons
+    // Today is first day of Git lessons
 
     //We are walking with baby steps now
     //At the end we can commit and push together
     //It can be easier but more boring than Java
+    //I will try by terminal section
+
 }
